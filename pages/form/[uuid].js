8bd57b3a1a17ccebeae6c04720b8cbf7da2c5a7f
sangho61 <hyunjin1012@gmail.com>
2022-01-18 19:49:20 -0800

    commit

--- pages/form/[uuid].js
+++ pages/form/[uuid].js
@@ -14,7 +14,7 @@ export async function getStaticPaths() {
   });
   return {
     paths: paths,
-    fallback: false,
+    fallback: true,
   };
 }
 
--- pages/form/response/[uuid].js
+++ pages/form/response/[uuid].js
@@ -12,7 +12,7 @@ export async function getStaticPaths() {
   });
   return {
     paths: paths,
-    fallback: false,
+    fallback: true,
   };
 }
 
